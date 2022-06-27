968d536d36c25f65425b35c8f2358d9cf86da0a6
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-27 22:17:46 +0200

    Add developers route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Landing from './components/pages/main/Landing';
 import Register from './components/pages/Register';
 import Login from './components/pages/Login';
+import Developers from './components/pages/Developers';
+import Posts from './components/pages/main/Posts';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/home/register" element={<Register />} />
         <Route path="/home/login" element={<Login />} />
+        <Route path="/home/developers" element={<Developers />} />
+        <Route path="/posts" element={<Posts />} />
       </Routes>
     </>
   );
