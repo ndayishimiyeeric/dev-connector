6c34a376299cc34bee6ea9d05962e5b97b3bc56a
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-28 18:02:43 +0200

    Add view profile fn

---
+++ src/components/Gitrepo.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+function Gitrepo(props) {
+  const {
+    name, stars, forks, watchers,
+  } = props;
+  return (
+    <div className="repo bg-white my-1 p-1">
+      <div>
+        <h4><a href="https://www.github.com">{name}</a></h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Tempora, alias.
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li className="badge badge-primary">
+            Stars:
+            &nbsp;
+            {stars}
+          </li>
+          <li className="badge badge-dark">
+            Watchers:
+            &nbsp;
+            {watchers}
+          </li>
+          <li className="badge">
+            Forks:
+            &nbsp;
+            {forks}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Gitrepo;
+
+Gitrepo.propTypes = {
+  name: PropTypes.string.isRequired,
+  stars: PropTypes.number.isRequired,
+  forks: PropTypes.number.isRequired,
+  watchers: PropTypes.number.isRequired,
+};
